44353ee643eea17e284c178e27222f58143ca909
Daniel Azamar <dazamar01@hotmail.com>
2022-06-29 20:59:03 -0500

    At '/' it renders <App>. At '/invoices' it renders <Invoices>.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Expenses from './expenses/expenses';
+import Invoices from './invoices/invoices';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +13,12 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter >
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+
     </BrowserRouter>
   </React.StrictMode>
 );
